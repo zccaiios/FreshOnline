8db53510707245daa8baa8ed690565d214943357
zhongchangcai <zccaiios@163.com>
2017-08-03 17:04:17 +0800

    创建账户类Passport

--- FreshOnline/db/base_model.py
+++ FreshOnline/db/base_model.py
@@ -5,6 +5,7 @@
 from django.db import models
 
 
+# 继承于models.Model
 class BaseModel(models.Model):
     '''
     抽象模型类
@@ -12,3 +13,7 @@ class BaseModel(models.Model):
     create_time = models.DateTimeField(auto_now_add=True, help_text='创建时间')
     update_time = models.DateTimeField(auto_now=True, help_text='更新时间')
     is_delete = models.BooleanField(default=False, help_text='删除标记')
+
+
+    class Meta:
+        abstract = True # 模型类是抽象的。
--- FreshOnline/df_user/models.py
+++ FreshOnline/df_user/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from db.base_model import BaseModel # 导入抽象模型类
 # Create your models here.
+
+
+
+class Passport(BaseModel):
+    '''
+    账户类
+    '''
+    username = models.CharField(max_length=20, help_text='用户名')
+    password = models.CharField(max_length=40, help_text='密码')
+    email = models.EmailField(help_text='邮箱')
+
+
+    class Meta:
+        db_table = 's_user_account' # 修改默认表名
